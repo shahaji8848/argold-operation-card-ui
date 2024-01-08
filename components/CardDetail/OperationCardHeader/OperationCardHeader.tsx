7bbc226cb30cea78d2b022de08b4177b14d192dd
monika8848 <monika@8848digital.com>
2024-01-08 15:33:58 +0530

    feat: UI changes in detail, list and search page

--- components/CardDetail/OperationCardHeader/OperationCardHeader.tsx
+++ components/CardDetail/OperationCardHeader/OperationCardHeader.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const OperationCardHeader = () => {
   return (
     <div className="d-flex justify-content-between spacing-mt p-0 ">
-      <p className="mb-0 m-0 p-0">
+      <p className="mb-0 m-0 p-0 mob-fs-14">
         Operation Card: <span className="bold">OP--Flatting-00014</span>{' '}
       </p>
       <button className="btn btn-secondary fs-13 px-4 px-1 btn-py">Back</button>
