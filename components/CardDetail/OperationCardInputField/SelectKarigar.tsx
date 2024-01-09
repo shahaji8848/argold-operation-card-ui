8bee325caefdd5a9199fbbda653bd998ad45ef44
priyanka8848 <priyanka@8848digital.com>
2024-01-09 15:53:48 +0530

    feat: made operation fields dynamics

--- components/CardDetail/OperationCardInputField/SelectKarigar.tsx
+++ components/CardDetail/OperationCardInputField/SelectKarigar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { use, useState } from 'react';
 import styles from '../../../styles/operationDetail.module.css';
 import { dataVal } from '../../../app/DataSet/operationCardProcessDept';
 const SelectKarigar = ({
@@ -6,10 +6,27 @@ const SelectKarigar = ({
   operationCardDetailData,
   operationCardKarigar,
 }: any) => {
-  const operationCardFields = Object.entries(dataVal[0])
+  const [operationCardFieldVal, setOperationCardFieldVal] = useState([
+    operationCardDetailData,
+  ]);
+  const operationCardFields: any = Object.entries(dataVal[0])
     .filter(([key, value]) => key.includes('show') && value === 1)
     .map(([key, value]) => key.replace('show_', ''));
-  console.log(operationCardFields, 'operationCardFields');
+
+  const operationCardFieldValue = Object.entries(
+    operationCardDetailData
+  ).filter(([key, value]) => operationCardFields.includes(key));
+
+  // function extractMatchingValues(obj: any, keys: any) {
+  //   console.log(keys, 'keys');
+  //   return Object.entries(obj).filter(([key]) => keys.includes(key));
+  // }
+
+  // const matchingValues = extractMatchingValues(
+  //   operationCardDetailData,
+  //   operationCardFields
+  // )
+  console.log(operationCardFieldValue, 'operationCardFieldValue');
   return (
     <div className={`row   text-center  py-2 mx-2 gap-3`}>
       <div className="col-md-2 p-0 m-0">
@@ -25,78 +42,37 @@ const SelectKarigar = ({
           </select>
         </div>
       </div>
-      <div className="col-md-2 p-0 m-0">
-        <div className="fs-14 bold text-start">Tounch Purity</div>
-        <div className="fs-14 ">
-          <input
-            type="text"
-            className="form-control  dark-blue inputFields"
-            id="inputText"
-            placeholder="Tounch Purity"
-            value={'0.0'}
-          />
-        </div>
-      </div>
-      <div className="col-md-2 p-0 m-0">
-        <div className="fs-14 bold text-start">Quantity</div>
-        <div className="fs-14 ">
-          <input
-            type="text"
-            className="form-control  dark-blue inputFields"
-            id="inputText"
-            placeholder="Tounch Purity"
-            value={'0.0'}
-          />
-        </div>
-      </div>
-      <div className="col-md-2 p-0 m-0">
-        <div className="fs-14 bold text-start">Quantity</div>
-        <div className="fs-14 ">
-          <input
-            type="text"
-            className="form-control  dark-blue inputFields"
-            id="inputText"
-            placeholder="Tounch Purity"
-            value={'0.0'}
-          />
-        </div>
-      </div>
-      <div className="col-md-2 p-0 m-0">
-        <div className="fs-14 bold text-start">Quantity</div>
-        <div className="fs-14 ">
-          <input
-            type="text"
-            className="form-control  dark-blue inputFields"
-            id="inputText"
-            placeholder="Tounch Purity"
-            value={'0.0'}
-          />
-        </div>
-      </div>
-      <div className="col-md-2 p-0 m-0">
-        <div className="fs-14 bold text-start">Quantity</div>
-        <div className="fs-14 ">
-          <input
-            type="text"
-            className="form-control  dark-blue inputFields"
-            id="inputText"
-            placeholder="Tounch Purity"
-            value={'0.0'}
-          />
-        </div>
-      </div>
-      <div className="col-md-2 p-0 m-0">
-        <div className="fs-14 bold text-start">Quantity</div>
-        <div className="fs-14 ">
-          <input
-            type="text"
-            className="form-control  dark-blue inputFields"
-            id="inputText"
-            placeholder="Tounch Purity"
-            value={'0.0'}
-          />
-        </div>
-      </div>
+      {operationCardFieldValue.length > 0 &&
+        operationCardFieldValue.map(([key, values]: any) => (
+          <div className="col-md-2 p-0 m-0">
+            <div className="fs-14 bold text-start">
+              {key
+                .split('_')
+                .map((val: any, index: any) => {
+                  return index === 0
+                    ? val.charAt(0).toUpperCase() + val.slice(1)
+                    : val;
+                })
+                .join(' ')}
+            </div>
+            <div className="fs-14 ">
+              <input
+                type="text"
+                className="form-control  dark-blue inputFields"
+                id="inputText"
+                placeholder={key
+                  .split('_')
+                  .map((val: any, index: any) => {
+                    return index === 0
+                      ? val.charAt(0).toUpperCase() + val.slice(1)
+                      : val;
+                  })
+                  .join(' ')}
+                value={values}
+              />
+            </div>
+          </div>
+        ))}
     </div>
     // <div>
     //   <div className="form-group row d-flex flex-column ">
