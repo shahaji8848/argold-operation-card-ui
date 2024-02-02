54afb0a5688d802bf84512cd1cbb727449e65f05
monika8848 <monika@8848digital.com>
2024-02-02 11:01:36 +0530

    added checkbox functionality

--- components/CardListing/OperationCardListingMaster.tsx
+++ components/CardListing/OperationCardListingMaster.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import OperationCardListingField from './OperationCardListingField';
 import OperationCardListingTable from './OperationCardListingTable';
 import useOperationCardList from '@/hooks/operation-card-list-hook/operation-card-list-hook';
 import { useRouter } from 'next/navigation';
+import { useSelector } from 'react-redux';
+import { get_access_token } from '@/store/slice/login-slice';
+import GETOperationCardListData from '@/services/api/operation-card-list-page/operation-card-list-api';
 
 const OperationCardListingMaster = () => {
   const router = useRouter();
@@ -10,7 +13,7 @@ const OperationCardListingMaster = () => {
     router.push('/');
   };
   const {
-    listData,
+    // listData,
     filtersData,
     handleInputChange,
     handleApplyFilters,
@@ -19,6 +22,77 @@ const OperationCardListingMaster = () => {
     URLForFiltersHandler,
     constructUrl,
   } = useOperationCardList();
+
+  const { token } = useSelector(get_access_token);
+  const [data, setData] = useState(filtersData);
+  const [showZeroBalance, setShowZeroBalance] = useState(false);
+  const [listData, setListData] = useState<any>([]);
+  const getOperationCardListFromAPI = async (url: string) => {
+    const getList: any = await GETOperationCardListData(url, token);
+    if (getList?.status === 200 && getList?.data?.message?.length > 0) {
+      setListData([...getList?.data?.message]);
+    } else {
+      setListData([]);
+    }
+    // setFiltersClear(0);
+  };
+
+  const handleCheckbox = () => {
+    // Toggle the value
+    setShowZeroBalance((prevShowZeroBalance) => !prevShowZeroBalance);
+    const url = new URL(window.location.href);
+
+    // et the search parameters
+    const searchParams = url.searchParams;
+    // Convert the search parameters to a string
+    const searchParamsString = searchParams.toString();
+
+    const keyValuePairs = searchParamsString?.split('&');
+
+    // Create an object to store the updated state
+    const updatedFiltersData: any = {
+      search: '',
+      name: '',
+      parent_melting_lot: '',
+      melting_lot: '',
+      product_purity: '',
+      product: '',
+      operation_department: '',
+      product_process_department: '',
+      karigar: '',
+      show_zero_balance: showZeroBalance ? 1 : 0,
+    };
+
+    keyValuePairs.forEach((keyValuePair) => {
+      const [key, value] = keyValuePair.split('=');
+      if (key in updatedFiltersData) {
+        // Replace '+' with space before updating the state
+        updatedFiltersData[key] = decodeURIComponent(value.replace(/\+/g, ' '));
+      }
+    });
+
+    // Update the state with the new values
+    setData((prevFiltersData: any) => ({
+      ...prevFiltersData,
+      ...updatedFiltersData,
+    }));
+
+    // if (showZeroBalance) {
+    //   // Toggle the value when the user checks the input box
+    const updatedShowZeroBalance = showZeroBalance ? '1' : '0';
+    searchParams.set('show_zero_balance', updatedShowZeroBalance);
+
+    // Trigger API call with the updated state
+    const updatedURL: any = url.search.split('?').pop();
+    console.log(updatedURL, 'updatedURL');
+    getOperationCardListFromAPI(updatedURL);
+    console.log('searchParamsStringsdsd', updatedURL);
+    // URLForFiltersHandler();
+    // } else {
+    //   getOperationCardListFromAPI(searchParamsString);
+    //   console.log('searchParamsString', searchParamsString);
+    // }
+  };
 
   return (
     <div className="container-fuild">
@@ -42,6 +116,8 @@ const OperationCardListingMaster = () => {
           handleKeyDownEnter={handleKeyDownEnter}
           URLForFiltersHandler={URLForFiltersHandler}
           constructUrl={constructUrl}
+          handleCheckbox={handleCheckbox}
+          showZeroBalance={showZeroBalance}
         />
         <div className="spacing-mt">
           <OperationCardListingTable data={listData} />
