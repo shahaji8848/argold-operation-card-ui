283ffa3ed1f35397cf29837655ddf14d0e615be5
monika8848 <monika@8848digital.com>
2024-02-09 13:20:36 +0530

    feat: API integration of Report loss table

--- components/Report/LossReport/LossReportTable.tsx
+++ components/Report/LossReport/LossReportTable.tsx
@@ -3,52 +3,58 @@ import React from 'react';
 const LossReportTable = ({ reportLossData }: any) => {
   return (
     <div className="table-responsive">
-      <table className="table table-bordered mt-2">
-        <thead className="card-listing-head ">
-          <tr>
-            {[
-              'type of loss',
-              'fine loss',
-              'total out weight',
-              'per kg loss',
-              'metal receive after recovery',
-              'recovered loss',
-              'per kg loss after recovery',
-              'unrecoverable loss',
-              'balance loss',
-              '% recovered',
-            ].map((val: any, index: any) => (
-              <th className=" text-center" scope="col" key={index}>
-                {val}
-              </th>
-            ))}
-          </tr>
-        </thead>
+      {reportLossData?.length > 0 && (
+        <table className="table table-bordered mt-2">
+          <thead className="card-listing-head ">
+            <tr>
+              {[
+                'type of loss',
+                'fine loss',
+                'total out weight',
+                'per kg loss',
+                'metal receive after recovery',
+                'recovered loss',
+                'per kg loss after recovery',
+                'unrecoverable loss',
+                'balance loss',
+                '% recovered',
+              ].map((val: any, index: any) => (
+                <th className=" text-center" scope="col" key={index}>
+                  {val}
+                </th>
+              ))}
+            </tr>
+          </thead>
 
-        <tbody className="card-listing-body">
-          {reportLossData?.length > 0 &&
-            reportLossData?.map((lossData: any, idx: any) => {
-              return (
-                <tr key={idx}>
-                  <td>{lossData?.type_of_loss}</td>
-                  <td className="text-end">{lossData?.fine_loss}</td>
-                  <td className="text-end">{lossData?.total_out_weight}</td>
-                  <td className="text-end">{lossData?.per_kg_loss}</td>
-                  <td className="text-end">
-                    {lossData?.metal_recieved_after_recovery}
-                  </td>
-                  <td className="text-end">{lossData?.recovered_loss}</td>
-                  <td className="text-end">
-                    {lossData?.per_kg_loss_after_recovery}
-                  </td>
-                  <td className="text-end">{lossData?.uncrecoverable_loss}</td>
-                  <td className="text-end">{lossData?.balance_loss}</td>
-                  <td className="text-end">{lossData?.percentage_recovered}</td>
-                </tr>
-              );
-            })}
-        </tbody>
-      </table>
+          <tbody className="card-listing-body">
+            {reportLossData?.length > 0 &&
+              reportLossData?.map((lossData: any, idx: any) => {
+                return (
+                  <tr key={idx}>
+                    <td>{lossData?.type_of_loss}</td>
+                    <td className="text-end">{lossData?.fine_loss}</td>
+                    <td className="text-end">{lossData?.total_out_weight}</td>
+                    <td className="text-end">{lossData?.per_kg_loss}</td>
+                    <td className="text-end">
+                      {lossData?.metal_recieved_after_recovery}
+                    </td>
+                    <td className="text-end">{lossData?.recovered_loss}</td>
+                    <td className="text-end">
+                      {lossData?.per_kg_loss_after_recovery}
+                    </td>
+                    <td className="text-end">
+                      {lossData?.uncrecoverable_loss}
+                    </td>
+                    <td className="text-end">{lossData?.balance_loss}</td>
+                    <td className="text-end">
+                      {lossData?.percentage_recovered}
+                    </td>
+                  </tr>
+                );
+              })}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
