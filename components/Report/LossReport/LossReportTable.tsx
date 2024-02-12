ebbe182c3af3036ad98d4f6b4306c656c52c9e87
monika8848 <monika@8848digital.com>
2024-02-12 10:54:36 +0530

    fix: resolved total value NaN issue

--- components/Report/LossReport/LossReportTable.tsx
+++ components/Report/LossReport/LossReportTable.tsx
@@ -85,79 +85,78 @@ const LossReportTable = ({ reportLossData }: any) => {
         </thead>
 
         <tbody className="card-listing-body">
-          {reportLossData?.length > 0 &&
-            reportLossData?.map((lossData: any, idx: any) => {
-              return (
-                <tr key={idx}>
-                  <td>
-                    <Link
-                      href={`/report/loss-report?department_group=${lossData?.type_of_loss}`}
-                    >
-                      {lossData?.type_of_loss}
-                    </Link>
-                  </td>
-                  <td className="text-end">
-                    {lossData?.fine_loss && lossData?.fine_loss !== 0
-                      ? lossData?.fine_loss?.toFixed(3)
-                      : '--'}
-                    {/* {lossData?.fine_loss?.toFixed(3)} */}
-                  </td>
-                  <td className="text-end">
-                    {lossData?.total_out_weight &&
-                    lossData?.total_out_weight !== 0
-                      ? lossData?.total_out_weight?.toFixed(3)
-                      : '--'}
-                    {/* {lossData?.total_out_weight?.toFixed(3)} */}
-                  </td>
-                  <td className="text-end">
-                    {lossData?.per_kg_loss && lossData?.per_kg_loss !== 0
-                      ? lossData?.per_kg_loss?.toFixed(3)
-                      : '--'}
-                    {/* {lossData?.per_kg_loss?.toFixed(3)} */}
-                  </td>
-                  <td className="text-end">
-                    {lossData?.metal_recieved_after_recovery &&
-                    lossData?.metal_recieved_after_recovery !== 0
-                      ? lossData?.metal_recieved_after_recovery?.toFixed(3)
-                      : '--'}
-                    {/* {lossData?.metal_recieved_after_recovery?.toFixed(3)} */}
-                  </td>
-                  <td className="text-end">
-                    {lossData?.recovered_loss && lossData?.recovered_loss !== 0
-                      ? lossData?.recovered_loss?.toFixed(3)
-                      : '--'}
-                    {/* {lossData?.recovered_loss?.toFixed(3)} */}
-                  </td>
-                  <td className="text-end">
-                    {lossData?.per_kg_loss_after_recovery &&
-                    lossData?.per_kg_loss_after_recovery !== 0
-                      ? lossData?.per_kg_loss_after_recovery?.toFixed(3)
-                      : '--'}
-                    {/* {lossData?.per_kg_loss_after_recovery?.toFixed(3)} */}
-                  </td>
-                  <td className="text-end">
-                    {lossData?.uncrecoverable_loss &&
-                    lossData?.uncrecoverable_loss !== 0
-                      ? lossData?.uncrecoverable_loss?.toFixed(3)
-                      : '--'}
-                    {/* {lossData?.uncrecoverable_loss?.toFixed(3)} */}
-                  </td>
-                  <td className="text-end">
-                    {lossData?.balance_loss && lossData?.balance_loss !== 0
-                      ? lossData?.balance_loss?.toFixed(3)
-                      : '--'}
-                    {/* {lossData?.balance_loss?.toFixed(3)} */}
-                  </td>
-                  <td className="text-end">
-                    {lossData?.percentage_recovered &&
-                    lossData?.percentage_recovered !== 0
-                      ? lossData?.percentage_recovered?.toFixed(3)
-                      : '--'}
-                    {/* {lossData?.percentage_recovered?.toFixed(3)} */}
-                  </td>
-                </tr>
-              );
-            })}
+          {reportLossData?.map((lossData: any, idx: any) => {
+            return (
+              <tr key={idx}>
+                <td>
+                  <Link
+                    href={`/report/loss-report?department_group=${lossData?.type_of_loss}`}
+                  >
+                    {lossData?.type_of_loss}
+                  </Link>
+                </td>
+                <td className="text-end">
+                  {lossData?.fine_loss && lossData?.fine_loss !== 0
+                    ? lossData?.fine_loss?.toFixed(3)
+                    : '--'}
+                  {/* {lossData?.fine_loss?.toFixed(3)} */}
+                </td>
+                <td className="text-end">
+                  {lossData?.total_out_weight &&
+                  lossData?.total_out_weight !== 0
+                    ? lossData?.total_out_weight?.toFixed(3)
+                    : '--'}
+                  {/* {lossData?.total_out_weight?.toFixed(3)} */}
+                </td>
+                <td className="text-end">
+                  {lossData?.per_kg_loss && lossData?.per_kg_loss !== 0
+                    ? lossData?.per_kg_loss?.toFixed(3)
+                    : '--'}
+                  {/* {lossData?.per_kg_loss?.toFixed(3)} */}
+                </td>
+                <td className="text-end">
+                  {lossData?.metal_recieved_after_recovery &&
+                  lossData?.metal_recieved_after_recovery !== 0
+                    ? lossData?.metal_recieved_after_recovery?.toFixed(3)
+                    : '--'}
+                  {/* {lossData?.metal_recieved_after_recovery?.toFixed(3)} */}
+                </td>
+                <td className="text-end">
+                  {lossData?.recovered_loss && lossData?.recovered_loss !== 0
+                    ? lossData?.recovered_loss?.toFixed(3)
+                    : '--'}
+                  {/* {lossData?.recovered_loss?.toFixed(3)} */}
+                </td>
+                <td className="text-end">
+                  {lossData?.per_kg_loss_after_recovery &&
+                  lossData?.per_kg_loss_after_recovery !== 0
+                    ? lossData?.per_kg_loss_after_recovery?.toFixed(3)
+                    : '--'}
+                  {/* {lossData?.per_kg_loss_after_recovery?.toFixed(3)} */}
+                </td>
+                <td className="text-end">
+                  {lossData?.uncrecoverable_loss &&
+                  lossData?.uncrecoverable_loss !== 0
+                    ? lossData?.uncrecoverable_loss?.toFixed(3)
+                    : '--'}
+                  {/* {lossData?.uncrecoverable_loss?.toFixed(3)} */}
+                </td>
+                <td className="text-end">
+                  {lossData?.balance_loss && lossData?.balance_loss !== 0
+                    ? lossData?.balance_loss?.toFixed(3)
+                    : '--'}
+                  {/* {lossData?.balance_loss?.toFixed(3)} */}
+                </td>
+                <td className="text-end">
+                  {lossData?.percentage_recovered &&
+                  lossData?.percentage_recovered !== 0
+                    ? lossData?.percentage_recovered?.toFixed(3)
+                    : '--'}
+                  {/* {lossData?.percentage_recovered?.toFixed(3)} */}
+                </td>
+              </tr>
+            );
+          })}
 
           <tr className="table-text">
             <td className="font-weight-bold ">Total</td>
