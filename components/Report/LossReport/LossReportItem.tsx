bc610d74c5b1d14e870182f2cfcc233ab13d35c2
monika8848 <monika@8848digital.com>
2024-03-08 16:40:02 +0530

    feat: changes report item table header name

--- components/Report/LossReport/LossReportItem.tsx
+++ components/Report/LossReport/LossReportItem.tsx
@@ -21,7 +21,7 @@ const LossReportItem = ({
             {[
               'item',
               'Operation Card Weight',
-              'Material Issue Weight',
+              'Material Issued Weight',
               'balance',
               'Convert to Unrecoverable Loss',
             ].map((val: any, index: any) => (
