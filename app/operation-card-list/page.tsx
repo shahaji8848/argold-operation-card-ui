540a0ec933eca6a674bc51367d5f01fa085b9ea2
Karan <karan@8848digital.com>
2024-03-11 18:37:10 +0530

    wip: web sockets work in progress :k

--- app/operation-card-list/page.tsx
+++ app/operation-card-list/page.tsx
@@ -4,23 +4,26 @@ import OperationCardListingMaster from '@/components/CardListing/OperationCardLi
 import io from 'socket.io-client';
 
 const Page = () => {
-  useEffect(() => {
-    // const socket = io('http://localhost:5000/'); // Replace with your server URL
-    const socket = io('https://erp.ar-gold.in/'); // Replace with your server URL
-    // // Event listener for when the connection is established
-    socket.on('connect', () => {
-      console.log('Connected to WebSocket');
-    });
+  // useEffect(() => {
+  //   const socket = io('http://127.0.0.1:9000/', {
+  //     withCredentials: true,
+  //     reconnectionAttempts: 5,
+  //   }); // Replace with your server URL
+  //   // const socket = io('https://erp.ar-gold.in/'); // Replace with your server URL
+  //   // // Event listener for when the connection is established
+  //   socket.on('connect', () => {
+  //     console.log('Connected to WebSocket');
+  //   });
 
-    socket.on('serverData', (dataFromServer) => {
-      console.log('Data received from server:', dataFromServer);
-      // Handle the received data as needed in your React component
-    });
-    // // Event listener for receiving messages from the server
-    // socket.on('message', (data) => {
-    //   console.log('Received message:', data);
-    // });
-  }, []);
+  //   socket.on('serverData', (dataFromServer) => {
+  //     console.log('Data received from server:', dataFromServer);
+  //     // Handle the received data as needed in your React component
+  //   });
+  //   // // Event listener for receiving messages from the server
+  //   // socket.on('message', (data) => {
+  //   //   console.log('Received message:', data);
+  //   // });
+  // }, []);
   return (
     <>
       <OperationCardListingMaster />
