96774fd7b40dc07f5b8bf5e22e7cbb017944272d
NikitaD8848 <nikita.dhumal@8848digital.com>
2024-05-07 17:54:20 +0530

    fix: Incorrect factory name is detail loss report API payload

--- services/api/operation-card-report-loss/operation-card-report-detail.ts
+++ services/api/operation-card-report-loss/operation-card-report-detail.ts
@@ -7,7 +7,7 @@ const GETOperationCardReportLossDetail = async (
   loss_period: any,
   factory: any
 ) => {
-  const url = `${CONSTANTS.API_BASE_URL}${CONSTANTS.CUSTOM_API_PATH}/custom_app.custom_app.doctype.operation_card.operation_card.get_loss_details_by_department_group_api?department_group=${department_group}&financial_year=${financial_year}&loss_period=${loss_period}&factory=${'ARG ERP Software'}`;
+  const url = `${CONSTANTS.API_BASE_URL}${CONSTANTS.CUSTOM_API_PATH}/custom_app.custom_app.doctype.operation_card.operation_card.get_loss_details_by_department_group_api?department_group=${department_group}&financial_year=${financial_year}&loss_period=${loss_period}&factory=${factory}`;
 
   const getResponse: any = await callGetAPI(url, token);
   return getResponse;
