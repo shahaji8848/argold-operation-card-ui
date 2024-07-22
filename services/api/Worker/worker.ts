c33d072d7665df57d8e3ab54ddc83b59905f6553
monika8848 <monika@8848digital.com>
2024-07-22 15:31:24 +0530

    feat: added worker dropdown in chain modal and  edit customer functionality in sales order detail table

--- services/api/Worker/worker.ts
+++ services/api/Worker/worker.ts
@@ -3,7 +3,6 @@ import { CONSTANTS, callGetAPI } from '@/services/config/api-config';
 const GETWorkerList = async (token: string) => {
   const fields: any = ['name', 'worker'];
 
-  //staging1-arg-manufacturing.8848digitalerp.com/api/resource/Worker
   // const url = `${CONSTANTS.API_BASE_URL}${CONSTANTS.STANDARD_API_PATH}/Worker?fields=${JSON.stringify(fields)}&limit=None`;
   const url = `${CONSTANTS.API_BASE_URL}${CONSTANTS.STANDARD_API_PATH}/Worker`;
 
