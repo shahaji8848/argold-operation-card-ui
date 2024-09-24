c380b9fb724a5f1349555fdd77ff72d326d4b140
monika8848 <monika@8848digital.com>
2024-09-24 13:57:02 +0530

    feat:added tab and key up down selection functionality

--- components/MeltingLotDashboard/OperationCardFilterOptionSelect.tsx
+++ components/MeltingLotDashboard/OperationCardFilterOptionSelect.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import meltingStyle from '../../styles/melting-lot-data.module.css';
 
 const OperationCardFilterOptionSelect = ({ meltingFiltersList, filterOptions, handleFilterChange }: any) => {
@@ -15,15 +15,18 @@ const OperationCardFilterOptionSelect = ({ meltingFiltersList, filterOptions, ha
 
   const [filteredProductList, setFilteredProductList] = useState(meltingFiltersList?.product || []);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [highlightedIndex, setHighlightedIndex] = useState(0);
+  const inputRef = useRef<HTMLInputElement | null>(null);
 
   const handleInputChange = (e: any) => {
     const { value } = e.target;
     handleFilterChange(e);
 
     // Filter dropdown options based on input value
-    const filteredList = meltingFiltersList?.product.filter((product: string) =>
+    const filteredList = meltingFiltersList?.product?.filter((product: string) =>
       product.toLowerCase().includes(value.toLowerCase())
     );
+
     setFilteredProductList(filteredList);
     setIsDropdownOpen(true); // Show the dropdown options
   };
@@ -33,6 +36,56 @@ const OperationCardFilterOptionSelect = ({ meltingFiltersList, filterOptions, ha
     handleFilterChange({
       target: { name: 'product', value: option },
     });
+  };
+
+  // Show all options when the input field is focused
+  const handleInputFocus = () => {
+    setFilteredProductList(meltingFiltersList?.product || []);
+    setIsDropdownOpen(true); // Show the dropdown on focus
+  };
+
+  useEffect(() => {
+    // setInputValueAutoComplete({
+    //   name: initialValue,
+    //   value: initialValue,
+    // });
+    const handleOutsideClickAutoComplete = (event: any) => {
+      if (inputRef.current && !inputRef.current.contains(event.target as Node)) {
+        setIsDropdownOpen(false);
+      }
+    };
+
+    window.addEventListener('click', handleOutsideClickAutoComplete);
+
+    return () => {
+      window.removeEventListener('click', handleOutsideClickAutoComplete);
+    };
+  }, []);
+
+  // Handle keyboard navigation
+  const handleKeyDown = (e: React.KeyboardEvent) => {
+    if (isDropdownOpen) {
+      switch (e.key) {
+        case 'ArrowDown':
+          setHighlightedIndex((prevIndex: any) => (prevIndex < filteredProductList?.length - 1 ? prevIndex + 1 : prevIndex));
+          e.preventDefault(); // Prevent scrolling
+          break;
+        case 'ArrowUp':
+          setHighlightedIndex((prevIndex: any) => (prevIndex > 0 ? prevIndex - 1 : prevIndex));
+          e.preventDefault(); // Prevent scrolling
+          break;
+        case 'Enter':
+          if (highlightedIndex >= 0) {
+            handleOptionClick(filteredProductList[highlightedIndex]);
+          }
+          break;
+        case 'Escape':
+          setIsDropdownOpen(false); // Close dropdown on escape
+          break;
+        default:
+          break;
+      }
+    }
   };
 
   return (
@@ -79,19 +132,29 @@ const OperationCardFilterOptionSelect = ({ meltingFiltersList, filterOptions, ha
                 name="product"
                 value={filterOptions?.product}
                 onChange={handleInputChange}
-                onFocus={() => setIsDropdownOpen(true)} // Show dropdown when focused
+                onFocus={handleInputFocus} // Show dropdown when focused
                 placeholder="Search product"
+                ref={inputRef}
+                onKeyDown={handleKeyDown}
               />
-              <div className={`${meltingStyle.custom_dropdown_options} ${isDropdownOpen ? 'open' : ''}`}>
-                {filteredProductList?.map((list: string, idx: number) => (
-                  <div key={idx} className={meltingStyle.custom_dropdown_option} onClick={() => handleOptionClick(list)}>
-                    {list}
-                  </div>
-                ))}
-                {filteredProductList.length === 0 && (
-                  <div className={`${meltingStyle.custom_dropdown_option} disabled`}>No options</div>
-                )}
-              </div>
+              {isDropdownOpen && (
+                <div className={`${meltingStyle.custom_dropdown_options}  ${isDropdownOpen ? meltingStyle.open : ''} `}>
+                  {filteredProductList?.map((list: string, idx: number) => (
+                    <div
+                      key={idx}
+                      className={`${meltingStyle.custom_dropdown_option} ${
+                        highlightedIndex === idx ? meltingStyle.highlighted : ''
+                      }`}
+                      onClick={() => handleOptionClick(list)}
+                    >
+                      {list}
+                    </div>
+                  ))}
+                  {filteredProductList?.length === 0 && (
+                    <div className={`${meltingStyle.custom_dropdown_option} disabled`}>No options</div>
+                  )}
+                </div>
+              )}
             </div>
           </div>
 
